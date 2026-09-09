1df5ecf818b49a1c37aabd5debd30018257344c0
Joel Baez <127694685+TheFunadou@users.noreply.github.com>
2026-09-09 10:02:00 -0600

    Configure CORS for production domains: igaproductos.com, api.igaproductos.com, adminpanel.igaproductos.com

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,12 +5,19 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins 'example.com'
-#
-#     resource '*',
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+require 'rack/cors'
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    # Define allowed origins from environment variable
+    # In production: igaproductos.com, api.igaproductos.com, adminpanel.igaproductos.com
+    # In development: localhost variants
+    origins ENV.fetch('CORS_ORIGINS', 'localhost:3000,127.0.0.1:3000').split(',')
+
+    resource '*',
+             headers: :any,
+             methods: [:get, :post, :put, :patch, :delete, :options, :head],
+             credentials: true,
+             max_age: 3600
+  end
+end
