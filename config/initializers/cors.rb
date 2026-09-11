7e6d6810a94bdd2b708ab37e89e493625ede62e4
Joel Baez <127694685+TheFunadou@users.noreply.github.com>
2026-09-11 10:23:18 -0600

    Remove credentials option from CORS configuration

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -17,7 +17,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     resource '*',
       headers: :any,
       methods: %i[get post put patch delete options],
-      credentials: true,
       max_age: 3600
   end
 end
