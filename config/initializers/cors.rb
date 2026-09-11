0b5de88dbf4eca5a89495ec5dce49f360d4c9763
Joel Baez <127694685+TheFunadou@users.noreply.github.com>
2026-09-11 10:22:56 -0600

    Modify CORS settings for improved security
    Updated CORS configuration to use HTTPS origins and added additional localhost origins.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,18 +2,21 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'igaproductos.com',
-            'www.igaproductos.com',
-            'api.igaproductos.com',
-            'adminpanel.igaproductos.com',
-            'sepomex.igaproductos.com',
-            'localhost:3000',
-            'localhost:3001',
-            '127.0.0.1'
+    origins(
+      'https://igaproductos.com',
+      'https://www.igaproductos.com',
+      'https://adminpanel.igaproductos.com',
+
+      # Desarrollo
+      'http://localhost:3000',
+      'http://localhost:3001',
+      'http://127.0.0.1:3000',
+      'http://127.0.0.1:3001'
+    )
 
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options],
+      methods: %i[get post put patch delete options],
       credentials: true,
       max_age: 3600
   end
